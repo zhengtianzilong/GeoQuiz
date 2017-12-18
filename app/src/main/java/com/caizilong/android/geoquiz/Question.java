6debd55538ae5784456d1d7577ebfad86c4c3a29
caizilong <603954378@qq.com>
2017-12-18 23:36:35 +0800

    First commit

---
+++ app/src/main/java/com/caizilong/android/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.caizilong.android.geoquiz;
+
+/**
+ * Created by Administrator on 2017/12/18.
+ */
+
+public class Question {
+
+    private int mTextResId;
+
+    private  boolean mAnswerTrue;
+
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
